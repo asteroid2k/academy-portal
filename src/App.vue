0907a6705b7a15d2d9af11c9de7ee97b4d4285c6
Kwaku Adusei Okyere <dobridobrev900@gmail.com>
2022-01-19 17:30:46 +0000

    axios header config

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
-<script></script>
+<script>
+import { mapGetters } from "vuex";
+import axios from "axios";
+
+export default {
+  name: "App",
+  computed: {
+    ...mapGetters(["token"]),
+    instance() {
+      return axios.create({
+        baseURL: "http://localhost:3009/api",
+        timeout: 3000,
+        headers: { Authorization: `token ${this.token}` },
+      });
+    },
+  },
+};
+</script>
 
 <template>
-  <router-view></router-view>
+  <router-view :instance="instance"></router-view>
 </template>
 
 <style></style>
